0ae9f323364bca270c5219e0e0a9f6e19f49f2df
emrys <emriesy@gmail.com>
2021-07-13 16:09:48 +0200

    CalculatorResult class

---
+++ src/main/java/com.foxminded.division/CalculatorResult.java
@@ -0,0 +1,37 @@
+package com.foxminded.division;
+
+import java.util.ArrayList;
+
+public class CalculatorResult {
+    private final int dividend;
+    private final int divider;
+    private final int result;
+    private ArrayList<Step> stepList;
+
+
+    public CalculatorResult(int dividend, int divider, int result) {
+        this.dividend = dividend;
+        this.divider = divider;
+        this.result = result;
+    }
+
+    public ArrayList<Step> getStepList() {
+        return stepList;
+    }
+
+    public int getDividend() {
+        return dividend;
+    }
+
+    public int getDivider() {
+        return divider;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setStepList(ArrayList<Step> stepList) {
+        this.stepList = stepList;
+    }
+}
